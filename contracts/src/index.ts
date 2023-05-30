ec44000cfa00713b3d66399406e4bc6647bf6d86
anandcsingh <anandcsingh@gmail.com>
2023-05-30 18:50:47 +0000

    feat: Add merkle map

--- contracts/src/index.ts
+++ contracts/src/index.ts
@@ -3,3 +3,11 @@ import { RankProof } from './RankProof.js';
 
 export { Add };
 export { RankProof };
+import { PrivateKey, PublicKey } from 'snarkyjs';
+import { HasMerkleMapContract } from './MerkleMapRank.js';
+interface Sender {
+  privateKey: PrivateKey;
+  publicKey: PublicKey;
+}
+
+export { HasMerkleMapContract, Sender };
