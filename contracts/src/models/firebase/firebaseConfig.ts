ba856bfd7d0a4839f986b2088973debb13008b77
anandcsingh <anandcsingh@gmail.com>
2023-07-18 03:21:18 +0000

    fix: Move firebase config

--- contracts/src/models/firebase/firebaseConfig.ts
+++ contracts/src/models/firebase/firebaseConfig.ts
@@ -1,14 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import Config from './config.js';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyDKlw-XbRKLRTjFZ05TjfMWceZ2GTtBxQw',
-  authDomain: 'rankproof.firebaseapp.com',
-  projectId: 'rankproof',
-  storageBucket: 'rankproof.appspot.com',
-  messagingSenderId: '479616737454',
-  appId: '1:479616737454:web:c29937cc4132262b63cd3f',
-};
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+export const app = initializeApp(Config);
 export const database = getFirestore(app);
