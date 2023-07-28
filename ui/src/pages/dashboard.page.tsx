87ce12420fdeee4df3040fab55894e7f1d1b222c
anandcsingh <anandcsingh@gmail.com>
2023-07-28 15:30:08 -0400

    fix: Fixed bug where QR code was opening when add student was clicked

--- ui/src/pages/dashboard.page.tsx
+++ ui/src/pages/dashboard.page.tsx
@@ -48,9 +48,9 @@ export default function Dashboard() {
 
                 <div className="grid grid-cols-2 gap-4 sm:grid-cols-3">
                   {/* Open the modal using ID.showModal() method */}
-                  <a onClick={showAddressModal}
+                  <a
                     className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
-                    href="#"
+                    href="#my_modal_8"
                   >
                   <div className="modal" id="my_modal_8">
                     <div className="modal-box bg-white">
