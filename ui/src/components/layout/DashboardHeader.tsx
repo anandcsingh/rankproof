b5cbea0a90279b8042fd7656450cb6efe0682d55
Nicholas Gray <nicholas_gray@live.com>
2023-07-31 08:40:45 -0400

    added profile to dashboard

--- ui/src/components/layout/DashboardHeader.tsx
+++ ui/src/components/layout/DashboardHeader.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 import Authentication from '@/modules/Authentication';
 
 const Header = () => {
- 
+
   return (
 
     <nav className="bg-gray-800">
@@ -19,16 +19,23 @@ const Header = () => {
               <img src="/assets/images/logo.png" className="h-6 mr-3 sm:h-9" alt="RankProok Logo" />
               <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">RankProof</span>
             </a>
-           
-            
-              <div className="flex items-center lg:order-2" id="mobile-menu-2">
-                {/* <Link 
+
+
+            <div className="flex items-center lg:order-2" id="mobile-menu-2">
+              {/* <Link 
                 className="inline-block py-2 px-4 text-sm leading-5 text-green-50 btn-primary hover:bg-indigo-600 font-medium focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 rounded-md float-right"
                  href='/dashboard' >
                     Launch App
                 </Link> */}
-              </div>
-            
+              <p className='mr-2'>Smoo Doe</p>
+              <button type="button" className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-primary rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+  Notifications
+  <span className="inline-flex items-center justify-center w-4 h-4 ml-2 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">
+    2
+  </span>
+</button>
+            </div>
+
 
           </div>
         </nav>
--- ui/src/components/sections/DashboardContainer.tsx
+++ ui/src/components/sections/DashboardContainer.tsx
@@ -64,7 +64,7 @@ const DashboardContainer: React.FC<DashboardContainerProps> = ({ showDummyData, 
             <section className="bg-white place-self-center lg:col-span-7 space-y-8">
                 <div className="m-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
                     <NotificationBox />
-                    <div className="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-16">
+                    <div>
                         {disciplinesLoaded &&
                             <DashboardProfile disciplines={disciplines} />}
                         {!disciplinesLoaded &&
